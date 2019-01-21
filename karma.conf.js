8836a17423279a65f0835747a4f348173c8d803b
Edgar S. Hurtado <edgar.sanchez@madisonmk.com>
2019-01-21 07:59:11 +0100

    Install testing suite

---
+++ karma.conf.js
@@ -0,0 +1,27 @@
+var webpackConfig = require('./webpack.config.js')
+
+module.exports = (config) => {
+    config.set({
+        // ... normal karma configuration
+        files: [
+            // all files ending in "_test"
+            { pattern: 'test/*_test.js', watched: false },
+            { pattern: 'test/**/*_test.js', watched: false }
+            // each file acts as entry point for the webpack configuration
+        ],
+
+        preprocessors: {
+            // add webpack as preprocessor
+            'test/*_test.js': [ 'webpack' ],
+            'test/**/*_test.js': [ 'webpack' ]
+        },
+
+        webpack: webpackConfig,
+
+        webpackMiddleware: {
+            // webpack-dev-middleware configuration
+            // i. e.
+            stats: 'errors-only'
+        }
+    })
+}
