cb41d2955b0f1f30c77346014eeb828e0e3dc9c2
Edgar S. Hurtado <edsanhu@gmail.com>
2019-02-21 07:00:25 +0100

    Create DefaultController

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,23 @@
+<?php 
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class DefaultController extends AbstractController{
+    /**
+     * @Route("/")
+     */
+    public function index(){
+        $apppath = __DIR__.'/../../public/index.html';
+        $response = new Response();
+
+        $response->setContent(file_get_contents($apppath));
+        $response->headers->set('Content-Type', 'text/html');
+        $response->setStatusCode(Response::HTTP_OK);
+
+        return $response;
+    }
+}
+?>
