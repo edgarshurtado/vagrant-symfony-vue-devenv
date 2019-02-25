4ba4dff2656d1dc98803345824f706b54d781257
Edgar S. Hurtado <edsanhu@gmail.com>
2019-02-25 07:37:49 +0100

    Add mock experiments

---
+++ tests/learning_tests/LearningMockTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Tests\LearningTests;
+
+use PHPUnit\Framework\TestCase;
+use App\Repository\UserRepository;
+use App\Entity\User;
+use Doctrine\Common\Persistence\ObjectRepository;
+
+class LearningMockTest extends TestCase{
+    public function testMockedFunctionIsCalledOnce(){
+        $user = new User();
+        $user->setEmail('fake_user@fake_domain.com');
+
+        $userRepository = $this->createMock(ObjectRepository::class);
+        $userRepository->expects($this->once())
+            ->method('find')
+            ->willReturn($user);
+
+        $userFromRepository = $userRepository->find(1);
+        $this->assertEquals('fake_user@fake_domain.com', $userFromRepository->getEmail());
+
+    }
+     public function testMockedFunctionIsCalledSeveralTimes(){
+        $user = new User();
+        $user->setEmail('fake_user@fake_domain.com');
+
+        $userRepository = $this->createMock(ObjectRepository::class);
+        $userRepository->expects($this->any())
+            ->method('find')
+            ->willReturn($user);
+
+        $userFromRepository;
+        for ($x = 0; $x <= 4; $x++){
+            $userFromRepository = $userRepository->find(1);
+        }
+
+        $this->assertEquals('fake_user@fake_domain.com', $userFromRepository->getEmail());
+
+     }
+}
