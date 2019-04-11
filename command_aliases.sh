5ab3d11086a96911553db4f167319c16c38801e4
Edgar S. Hurtado <edsanhu@gmail.com>
2019-04-11 08:09:01 +0200

    Create route for getting login form. WIP

--- command_aliases.sh
+++ command_aliases.sh
@@ -8,6 +8,14 @@ stop_db(){
     sudo docker stop plock_mysql 
 }
 
+run_server(){
+    php bin/console server:start 0.0.0.0:8080
+}
+
+stop_server(){
+    php bin/console server:stop
+}
+
 regenerate_db(){
     php bin/console doctrine:schema:drop --force --no-interaction
     php bin/console doctrine:schema:update --force --no-interaction
