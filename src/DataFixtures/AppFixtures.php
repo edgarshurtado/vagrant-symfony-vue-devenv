91af45f6582e94c942c082478cd2e59561c8d4ed
Edgar S. Hurtado <edsanhu@gmail.com>
2019-04-11 08:08:37 +0200

    Use repository function to persist user fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -10,26 +10,27 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+        $user_repository = $manager->getRepository(User::class);
+
         $user = new User();
         $user->setEmail('robe@fake_domain.com');
         $user->setPassword('test');
-        $manager->persist($user);
+        $user_repository->create($user);
 
         $user = new User();
         $user->setEmail('sara@fake_domain.com');
         $user->setPassword('test');
-        $manager->persist($user);
+        $user_repository->create($user);
 
         $user = new User();
         $user->setEmail('ana@fake_domain.com');
         $user->setPassword('test');
-        $manager->persist($user);
+        $user_repository->create($user);
 
         $user = new User();
         $user->setEmail('javi@fake_domain.com');
         $user->setPassword('test'); 
         $manager->persist($user);
 
-        $manager->flush();
     }
 }
